b0fe5c3a52fc50914675c185d906cba2389edd73
Forrest Fleming <ffleming@gmail.com>
2015-05-17 10:47:46 -0700

    Move it_understands to method instead of shared examples
    style

--- spec/dice_string_spec.rb
+++ spec/dice_string_spec.rb
@@ -1,16 +1,14 @@
 require 'spec_helper'
-RSpec.configure do |c|
-  c.alias_it_should_behave_like_to :it_understands, 'can understand'
+
+def it_understands(dice_string)
+  it "doesn't raise an error on #{dice_string}'" do
+    expect { DMTool::Parser::DiceString.new(dice_string) }.to_not raise_error
+  end
 end
 
 describe DMTool::Parser::DiceString do
   let(:implicit) { DMTool::Parser::DiceString.new 'd6' }
   let(:dice) { DMTool::Parser::DiceString.new '3d6' }
-  shared_examples_for 'dice string' do |dice_string|
-    it "'#{dice_string}'" do
-      expect { DMTool::Parser::DiceString.new("#{dice_string}")}.to_not raise_error
-    end
-  end
 
   describe '#initialize' do
     it 'Raises an error for bad values' do
@@ -18,13 +16,13 @@ describe DMTool::Parser::DiceString do
     end
   end
 
-  it_understands 'dice string', '2d6'
-  it_understands 'dice string', 'D20'
-  it_understands 'dice string', '4e8'
-  it_understands 'dice string', '100E12'
-  it_understands 'dice string', '4f'
-  it_understands 'dice string', '2F'
-  it_understands 'dice string', '5dF'
-  it_understands 'dice string', '5d6+2'
+  it_understands '2d6'
+  it_understands 'D20'
+  it_understands '4e8'
+  it_understands '100E12'
+  it_understands '4f'
+  it_understands '2F'
+  it_understands '5dF'
+  it_understands '5d6+2'
 
 end
