2b0ce06b57706b23ee25d8ad8b4ae7f8723cb060
Forrest Fleming <ffleming@gmail.com>
2015-05-17 11:26:28 -0700

    Parser code for directives

--- lib/dmtool/parser.rb
+++ lib/dmtool/parser.rb
@@ -7,11 +7,12 @@ class DMTool::Parser
   def parse!(input)
     history.push input
     command, remainder = input.split(/ /, 2)
+    dice_string, directives_string = remainder.to_s.split(',', 2)
     case command
     when 'roll'
-      roll remainder
+      roll dice_string, directives_string
     when 'raw'
-      raw remainder
+      raw dice_string, directives_string
     when 'exit'
       exit
     else
@@ -19,18 +20,28 @@ class DMTool::Parser
     end
   end
 
-  def roll(dice_string)
+  def roll(dice_string, directives_string='')
     dice = DMTool::Parser::DiceString.new(dice_string)
+    directives = directives_from(directives_string)
     result = DMTool::Roller.sum(*dice.dice)
-    dice.modifier.call(result)
+    result = dice.modifier.call(result)
   end
 
-  def raw(dice_string)
+  def raw(dice_string, directives_string='')
     dice = DMTool::Parser::DiceString.new(dice_string)
+    directives = directives_from(directives_string)
     DMTool::Roller.roll(*dice.dice)
   end
 
   alias parse parse!
+
   private
 
+  def directives_from(directives_string)
+    directives = directives_string.to_s.split ','
+    directives.map! { |str| DMTool::Parser::Directive.new(str) }
+  end
+
+  def dice_from(dice_string)
+  end
 end
