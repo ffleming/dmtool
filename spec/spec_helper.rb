5d1b3aca1e4b8c27f0a245b7a3f7c7284f5ac11f
Forrest Fleming <ffleming@gmail.com>
2015-09-27 17:56:09 -0700

    Automate code climate reporting

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,14 @@
+def key_location
+  spec = Gem::Specification.find_by_name('dmtool')
+  File.join(spec.gem_dir, '.code_climate.secret')
+end
+
+def set_code_climate_key!
+  ENV['CODECLIMATE_REPO_TOKEN'] = File.read key_location rescue nil
+end
+
+set_code_climate_key!
+
 require 'simplecov'
 require 'codeclimate-test-reporter'
 SimpleCov.start do
